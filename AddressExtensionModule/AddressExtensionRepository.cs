8ac9ed0586a7255048c0089c55b2c485cdcc621d
Andrey Kozlov <yecli5@gmail.com>
2019-04-16 10:33:31 +0500

    Address extension problem fixes: - Registered ICustomerRepository (root cause of problem); - ICustomerRepository should be inherited; - Fixed AddressExtensions query for getting for required members; - Added filterting based on ResponseGroup; - Updated nuget packages, aligned them with dependencies in module.manifest; - Re-scaffold migration to have CusotmerModule changes got via update;

--- AddressExtensionModule/AddressExtensionRepository.cs
+++ AddressExtensionModule/AddressExtensionRepository.cs
@@ -3,46 +3,54 @@ using System.Linq;
 using AddressExtensionModule.Model;
 using VirtoCommerce.CustomerModule.Data.Model;
 using VirtoCommerce.CustomerModule.Data.Repositories;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Data.Infrastructure.Interceptors;
 
 namespace AddressExtensionModule
 {
-    public class AddressExtensionRepository : MemberRepositoryBase
-    {
-        public AddressExtensionRepository()
-        {
-        }
+	public class AddressExtensionRepository : CustomerRepositoryImpl
+	{
+		public AddressExtensionRepository()
+		{
+		}
 
-        public AddressExtensionRepository(string nameOrConnectionString, params IInterceptor[] interceptors)
-            : base(nameOrConnectionString, interceptors)
-        {
-            Configuration.ProxyCreationEnabled = false;
-        }
+		public AddressExtensionRepository(string nameOrConnectionString, params IInterceptor[] interceptors)
+			: base(nameOrConnectionString, interceptors)
+		{
+			Configuration.ProxyCreationEnabled = false;
+		}
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            #region AddressExtension
-            modelBuilder.Entity<AddressExtensionDataEntity>().HasKey(x => x.Id)
-                .Property(x => x.Id);
-            modelBuilder.Entity<AddressExtensionDataEntity>().ToTable("AddressShippingExtension");
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			#region AddressExtension
+			modelBuilder.Entity<AddressExtensionDataEntity>().HasKey(x => x.Id)
+				.Property(x => x.Id);
+			modelBuilder.Entity<AddressExtensionDataEntity>().ToTable("AddressShippingExtension");
 
-            #endregion
+			#endregion
 
-            base.OnModelCreating(modelBuilder);
-        }
+			base.OnModelCreating(modelBuilder);
+		}
 
-	    public IQueryable<AddressExtensionDataEntity> AddressExtensions
-	    {
-		    get { return GetAsQueryable<AddressExtensionDataEntity>(); }
-	    }
+		public IQueryable<AddressExtensionDataEntity> AddressExtensions
+		{
+			get { return GetAsQueryable<AddressExtensionDataEntity>(); }
+		}
 
-	    public override MemberDataEntity[] GetMembersByIds(string[] ids, string responseGroup = null, string[] memberTypes = null)
-	    {
-		    var retVal = base.GetMembersByIds(ids, responseGroup, memberTypes);
+		public override MemberDataEntity[] GetMembersByIds(string[] ids, string responseGroup = null, string[] memberTypes = null)
+		{
+			var retVal = base.GetMembersByIds(ids, responseGroup, memberTypes);
 
-		    var addresses = AddressExtensions.Where(x => ids.Contains(x.Id)).ToArray();
+			var memberResponseGroup = EnumUtility.SafeParseFlags(responseGroup, MemberResponseGroup.Full);
 
-		    return retVal;
-	    }
-    }
+			// Need to load Addresses only if we have this response group included
+			// https://github.com/VirtoCommerce/vc-module-customer/blob/dev/VirtoCommerce.CustomerModule.Data/Repositories/MemberRepositoryBase.cs#L257
+			if (memberResponseGroup.HasFlag(MemberResponseGroup.WithAddresses))
+			{
+				var addresses = AddressExtensions.Where(x => ids.Contains(x.MemberId)).ToArray();
+			}
+
+			return retVal;
+		}
+	}
 }
